992e4c9d4e4579915f8b0928161ce04f4fca13b7
angelobagasbas <33994486+angelobagasbas@users.noreply.github.com>
2018-03-13 08:49:58 +0800

    💦 made db name constant

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
 //set off all error for security purposes
 error_reporting(E_ALL);
-define( "DB_DSN", "mysql:host=localhost;port=3306;dbname=test" );
+define( "DB_NAME", "it380" );
 define( "DB_HOST", "localhost" );
 define( "DB_USERNAME", "root" );
 define( "DB_PASSWORD", "" );
 define( "DB_TABLE", "test" );
+
+define( "DB_DSN", "mysql:host=localhost;port=3306;dbname=".DB_NAME."" );
 
 try{
     $con = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD, array(PDO::MYSQL_ATTR_LOCAL_INFILE => true));
